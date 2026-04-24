b1e885ae3cc658cf1c21252369647bab6ce87859
teste <05rebecacastro@gmail.com>
2026-04-24 18:00:37 -0300

    repetição
    questão incompleta

--- src/Repetição/questao1.ts
+++ src/Repetição/questao1.ts
@@ -1,1 +1,11 @@
-oi
+// 1. Crie um programa que peça números até o usuário digitar 0 e mostre:
+//  Quantidade de números digitados
+//  Soma total
+
+let cont:number = 0;
+let acum:number = 0;
+let num1:number = Number(prompt("digite um numero"));
+
+while (num1 != 0){
+    
+}
